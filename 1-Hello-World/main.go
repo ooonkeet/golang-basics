3adc3a74362905e2164071bbaf2a919d674f6f7e
Ankit <ankitspare981@gmail.com>
2025-12-03 17:21:50 +0530

    001

---
+++ 1-Hello-World/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// package - multiple files of go kept in logical group
+// main() is the entry point
+// fmt -> std.lib package used for formatting
+
+
+// to build a file -> go build {filename}.go
+
+// to run a file -> ./{filename}
+
+// direct build and run -> go run {filename}.go
