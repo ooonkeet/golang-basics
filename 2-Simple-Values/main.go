d1a24bc9de74df15f98f3695b5eb6d5227f1e560
Ankit <ankitspare981@gmail.com>
2025-12-05 13:48:08 +0530

    Simple Values

---
+++ 2-Simple-Values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int
+	fmt.Println(1)
+	fmt.Println(5+6)
+	// string
+	fmt.Println("HI this is Ankit")
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+	//float
+	fmt.Println(10.5)
+	fmt.Println(7.0/3.0)
+	//type coercion
+	fmt.Println(7.0/3)
+	fmt.Println(7/3)
+}
+
+// data types -> int, string, bool, float
